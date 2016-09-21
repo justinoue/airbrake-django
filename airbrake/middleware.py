ef609036afdcf5d11f7ff6b500fb8e7d2fd6b1d5
Luke Seelenbinder <me@lukeseelenbinder.com>
2016-09-21 14:38:32 -0400

    More fixes for 1.10.

--- airbrake/middleware.py
+++ airbrake/middleware.py
@@ -4,8 +4,10 @@ from django.utils.deprecation import MiddlewareMixin
 from airbrake.utils.client import Client
 
 class AirbrakeNotifierMiddleware(MiddlewareMixin):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.client = Client()
+
+        super(AirbrakeNotifierMiddleware, self).__init__(*args, **kwargs)
 
     def process_exception(self, request, exception):
         if hasattr(settings, 'AIRBRAKE') and not settings.AIRBRAKE.get('DISABLE', False):
