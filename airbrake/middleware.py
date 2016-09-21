a918aa5be14ea7652b12f412d32134c8283f310a
Luke Seelenbinder <me@lukeseelenbinder.com>
2016-09-21 14:22:28 -0400

    Use Django's MiddlewareMixin to correct bugs with 1.10.

--- airbrake/middleware.py
+++ airbrake/middleware.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.core.exceptions import MiddlewareNotUsed
+from django.utils.deprecation import MiddlewareMixin
 from airbrake.utils.client import Client
 
-class AirbrakeNotifierMiddleware(object):
-    def __init__(self, **kwargs):
+class AirbrakeNotifierMiddleware(MiddlewareMixin):
+    def __init__(self):
         self.client = Client()
 
     def process_exception(self, request, exception):
